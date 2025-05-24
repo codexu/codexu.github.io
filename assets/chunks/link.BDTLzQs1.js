41997ba29e0ed8c929716c2e96ddb691b224f4de
codexu <codexu@users.noreply.github.com>
2025-05-24 00:49:05 +0000

    deploy: 97f137327070923c263094f12dc2ead6e4150a79

---
+++ assets/chunks/link.BDTLzQs1.js
@@ -0,0 +1,26 @@
+import{ad as c}from"./framework.Cthp9TdA.js";/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase(),h=e=>e.replace(/^([A-Z])|[\s-_]+(\w)/g,(t,r,o)=>o?o.toUpperCase():r.toLowerCase()),w=e=>{const t=h(e);return t.charAt(0).toUpperCase()+t.slice(1)},p=(...e)=>e.filter((t,r,o)=>!!t&&t.trim()!==""&&o.indexOf(t)===r).join(" ").trim();/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var a={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor","stroke-width":2,"stroke-linecap":"round","stroke-linejoin":"round"};/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const C=({size:e,strokeWidth:t=2,absoluteStrokeWidth:r,color:o,iconNode:l,name:s,class:g,...d},{slots:n})=>c("svg",{...a,width:e||a.width,height:e||a.height,stroke:o||a.stroke,"stroke-width":r?Number(t)*24/Number(e):t,class:p("lucide",...s?[`lucide-${i(w(s))}-icon`,`lucide-${i(s)}`]:["lucide-icon"]),...d},[...l.map(u=>c(...u)),...n.default?[n.default()]:[]]);/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=(e,t)=>(r,{slots:o})=>c(C,{...r,iconNode:t,name:e},o);/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=k("link",[["path",{d:"M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71",key:"1cjeqo"}],["path",{d:"M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71",key:"19qd67"}]]);export{f as L,k as c};
