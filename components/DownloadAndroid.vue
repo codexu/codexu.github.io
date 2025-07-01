6f0b1e91362d4e829f463d860b11507db55b2cb4
codexu <461229187@qq.com>
2025-07-01 19:39:38 +0800

    feat: add Android download component with APK links for different architectures

--- components/DownloadAndroid.vue
+++ components/DownloadAndroid.vue
@@ -5,9 +5,9 @@
 <script lang="ts" setup>
 import DownloadGroup from './DownloadGroup.vue';
 const buttons = [
-  { name: 'Android arm', url: 'http://file.notegen.top/app-arm-release-unsigned.apk' },
-  { name: 'Android arm64', url: 'http://file.notegen.top/app-arm64-release-unsigned.apk' },
-  { name: 'Android x86', url: 'http://file.notegen.top/app-x86-release-unsigned.apk' },
-  { name: 'Android x86_64', url: 'http://file.notegen.top/app-x86_64-release-unsigned.apk' },
+  { name: 'Android arm', url: 'http://file.notegen.top/app-arm-release.apk' },
+  { name: 'Android arm64', url: 'http://file.notegen.top/app-arm64-release.apk' },
+  { name: 'Android x86', url: 'http://file.notegen.top/app-x86-release.apk' },
+  { name: 'Android x86_64', url: 'http://file.notegen.top/app-x86_64-release.apk' },
 ]
 </script>
